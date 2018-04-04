f108409b864a6641205bb32865da0066bba44acc
mchayapol <mchayapol@gmail.com>
2018-04-04 11:58:52 +0700

    Improve to Python 3

--- disjointsets.py
+++ disjointsets.py
@@ -1,9 +1,9 @@
 
 class DisjointSets:
     def __init__(self, n):
-        self.p = range(n)
-        self.rank = [0]*n
-        
+        self.p = list(range(n))
+        self.rank = [0] * n
+
     def findset(self, u):
         if self.p[u] == u:
             return u
@@ -11,7 +11,7 @@ class DisjointSets:
             self.p[u] = self.findset(self.p[u])
             return self.p[u]
 
-    def union(self, u,v):
+    def union(self, u, v):
         a = self.findset(u)
         b = self.findset(v)
         if self.rank[a] < self.rank[b]:
@@ -20,5 +20,3 @@ class DisjointSets:
             self.p[b] = a
             if self.rank[a] == self.rank[b]:
                 self.rank[a] += 1
-
-
